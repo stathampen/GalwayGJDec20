8487c3c2b9baa462a90fdc45de45c55f40e7e647
E. Hannon <blackmagemario@gmail.com>
2020-12-12 16:03:19 +0000

    create elements needed for basic gameplay loop

--- Assets/Scripts/Potions/PotionFiller.cs
+++ Assets/Scripts/Potions/PotionFiller.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class PotionFiller : MonoBehaviour
 {
-	[SerializeField] private Potion[] potions;
+	[SerializeField] private Potion potion;
 	private void OnTriggerEnter(Collider other)
 	{
 		var bottle = other.GetComponent<Bottle>();
 		if (bottle)
 		{
-			bottle.SetPotion(0);
+			for (var i = 0; i < bottle.potions.Length; i++)
+			{
+				if (bottle.potions[i].potionName == potion.potionName)
+				{
+					bottle.SetPotion(i);
+				}
+			}
 			var potionBehaviour = bottle.gameObject.AddComponent<PotionBehaviour>();
 			potionBehaviour.potionMaterials = new Material[bottle.potions.Length];
 			for (var i = 0; i < bottle.potions.Length; i++)
