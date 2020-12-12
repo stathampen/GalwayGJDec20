8b43af5c1ce40be387f71c0b00c338d2c26c9618
stathampen <33280323+stathampen@users.noreply.github.com>
2020-12-12 16:38:36 +0000

    starting to mix behaviour

---
+++ Assets/Scripts/Potions/MixerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixerScript : MonoBehaviour
+{
+    private Material potionMaterial;
+
+    //when the potion enters the spot need to check the potions material
+    private void OnTriggerEnter(Collider other) {
+        
+        if(other.gameObject.tag == "PotionBottle")
+        {
+            GameObject potionBottle = other.gameObject;
+
+            Debug.Log("This is a potion");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
